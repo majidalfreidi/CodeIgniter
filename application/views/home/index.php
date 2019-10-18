54e27d042180de58679b4bbe3fd6414ab9c7adec
Majid Alfredi <majidalfredi123@gmail.com>
2019-10-18 21:57:19 +0700

    Menambahkan data CodeIgniter

---
+++ application/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="jumbotron">
+    <h1 class="display-4">Welcome to My Website</h1>
+    <p class="lead">Hello, My name is Majid Alfreidi</p>
+    <hr class="my-4">
+    <p>This is my personal blog, i've to learning building a website with framework Codeigniter and bootstrap :)</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+</div>
---
+++ application/views/mahasiswa/index.php
@@ -0,0 +1,12 @@
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-6">
+            <ul class="list-group">
+                <h1>Daftar Mahasiswa</h1>
+                <li class="list-group-item">Cras justo odio</li>
+            </ul>
+        </div>
+    </div>
+
+</div>
