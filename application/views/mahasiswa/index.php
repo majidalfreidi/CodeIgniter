f3a7a23018b2986b01d15c93d09b342ca9f27ec9
majid Alfreidi <ketavank.city25@gmail.com>
2019-10-24 12:37:45 +0700

    Mengkoneksikan CI_App ke database

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -4,7 +4,9 @@
         <div class="col-md-6">
             <ul class="list-group">
                 <h1>Daftar Mahasiswa</h1>
-                <li class="list-group-item">Cras justo odio</li>
+                <?php foreach( $mahasiswa as $mhs ): ?>
+                    <li class="list-group-item"><?= $mhs['nama']; ?></li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </div>
